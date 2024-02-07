5e7231a114bccdf6575c9fd7ae7b2653db4257cb
k_fene_8 <mandrekarh25@gmail.com>
2024-02-07 13:45:10 +0530

    UI/UX: Display history of coordinates fetched.

--- lib/presentation/screens/coordinateHistory.dart
+++ lib/presentation/screens/coordinateHistory.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
+import 'package:mapsense/domain/RecordClearer.dart';
+import 'package:mapsense/domain/coordinateFetcher.dart';
 
 class CoordinateHistoryPage extends StatefulWidget {
   const CoordinateHistoryPage({Key? key}) : super(key: key);
@@ -12,24 +12,15 @@ class CoordinateHistoryPage extends StatefulWidget {
 class _CoordinateHistoryPageState extends State<CoordinateHistoryPage> {
   List<Map<String, dynamic>> _coordinates = [];
 
+
   @override
   void initState() {
     super.initState();
-    _fetchCoordinates();
-  }
-
-  Future<List<Map<String, dynamic>>> _fetchCoordinates() async {
-    final databasePath = await getDatabasesPath();
-    final database = await openDatabase(
-      join(databasePath, 'coordinateDatabase.db'),
-      version: 1,
-    );
-
-    final coordinates = await database.query('coordinates');
-    setState(() {
-      _coordinates = coordinates;
+    CoordinateFetcher().fetchCoordinates().then((value) {
+      setState(() {
+        _coordinates = value;
+      });
     });
-    return coordinates;
   }
 
   @override
@@ -40,10 +31,18 @@ class _CoordinateHistoryPageState extends State<CoordinateHistoryPage> {
         itemBuilder: (context, index) {
           final coordinate = _coordinates[index];
           return ListTile(
-            title: Text('Latitude: ${coordinate['latitude']}'),
-            subtitle: Text('Longitude: ${coordinate['longitude']}'),
+            title: Text('Record ${index+1}'),
+            subtitle: Text('Longitude: ${coordinate['longitude']} \nLatitude: ${coordinate['latitude']}'),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          clearRecords();
+            setState(() {
+      _coordinates = [];
+    });},
+        child: Icon(Icons.delete),
       ),
     );
   }
